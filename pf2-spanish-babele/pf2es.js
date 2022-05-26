092aaa43ff99a6e3e0e6becbe566a05a484b0cba
javier <javier@castroparga.com>
2022-05-26 17:14:16 +0200

    Registrar el compedio en babele.

--- pf2-spanish-babele/pf2es.js
+++ pf2-spanish-babele/pf2es.js
@@ -1,5 +1,10 @@
-/* globals Hooks, game */
+/* globals Hooks, game, Babele */
 
 Hooks.once("babele.ready", () => {
     game.pf2e.ConditionManager.initialize();
+    Babele.get().register({
+        module: "pf2-spanish-babele",
+        lang: "es",
+        dir: "compendiums"
+    });
 });
