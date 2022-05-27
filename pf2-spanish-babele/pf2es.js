881c840de4c5e09a4412c31beb869263a02d0b8d
Parvus Domus <parvusdomus@gmail.com>
2022-05-27 18:49:45 +0200

    cambio la funcion register de babele al init

--- pf2-spanish-babele/pf2es.js
+++ pf2-spanish-babele/pf2es.js
@@ -1,10 +1,20 @@
 /* globals Hooks, game, Babele */
+Hooks.on('init', () => {
+    console.log ("INITIALIZING SPANISH TRANSLATION BABELE INIT")
+    if(typeof Babele !== 'undefined') {
+        Babele.get().register({
+            module: 'pf2-spanish-babele',
+            lang: 'es',
+            dir: 'compendiums/es'
+        });
+    }
+});
 
 Hooks.once("babele.ready", () => {
     game.pf2e.ConditionManager.initialize();
-    Babele.get().register({
-        module: "pf2-spanish-babele",
-        lang: "es",
-        dir: "compendiums"
-    });
+  //  Babele.get().register({
+  //      module: "pf2-spanish-babele",
+  //      lang: "es",
+  //      dir: "compendiums/es"
+  //  });
 });
